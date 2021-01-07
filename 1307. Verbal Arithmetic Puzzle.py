22c6580314a6cb835d1e7e05f86a289ac8df32f0
thophan92 <huutho.bk10@gmail.com>
2021-01-08 01:49:47 +0900

    Update 1307. Verbal Arithmetic Puzzle.py
    24/32

--- 1307. Verbal Arithmetic Puzzle.py
+++ 1307. Verbal Arithmetic Puzzle.py
@@ -1,25 +1,32 @@
 class Solution:
     def isSolvable(self, words: List[str], result: str) -> bool:
-        chars = {c for word in words for c in word}
-        temp = {c for c in result}
-        chars |= temp
-        nz_chars = {word[0] for word in words}
-        last_chars = {word[-1] for word in words}
-        last_chars.add(result[-1])
-        nz_chars.add(result[0])
-        chars = list(last_chars) + list(chars - last_chars)
-        # print(last_chars)
-        # print(chars)
-        # chars = list(chars)
-        nz_chars = list(nz_chars)
-        #chars -= nz_chars
+        nz_chars = [word[0] for word in words]
+        nz_chars.append(result[0])
+        all_words = list(words)
+        all_words.append(result)
+        rev = lambda word : "".join([word[i] for i in range(len(word) - 1, -1, -1)])
+        all_words = [rev(word) for word in all_words]
+        longest = max([len(w) for w in all_words])
+        shortest = min([len(w) for w in all_words])
+        chars = []
+        for i in range(longest):
+            for w in all_words:
+                if i < len(w) and w[i] not in chars:
+                    chars.append(w[i])
+        milestone = {}
+        temp = set()
+        for i in range(shortest):
+            for word in all_words:
+                if i < len(word):
+                    temp.add(word[i])
+            milestone[len(temp) - 1] = i                
         config = {}
         self.ans = False
         chosen = [False] * 10
-        # print(chars)
-        # print(nz_chars)
         n = len(chars)
-
+        print(all_words)
+        print(chars)
+        print(milestone)
         def check(words, result):
             left = 0
             for word in words:
@@ -28,11 +35,21 @@ class Solution:
             return left == right
         
         def early_stop(k):
-            if k != len(last_chars) - 1:
+            if k not in milestone:
                 return False
-            left = str(sum([config[word[-1]] for word in words]))
-            right = str(config[result[-1]])
-            return left[-1] != right[-1]
+            k = milestone[k]
+            left = 0
+            print(config)
+            for word in words:
+                left += int("".join([str(config[w]) for w in word[max(-1 - k, -len(word)):]]))
+                print(word, left)
+            right = int("".join([str(config[w]) for w in result[max(-1 - k, -len(result)):]]))
+            left = str(left)
+            right = str(right)
+            print("right:", result, right)
+            stop = left[-k-1:] != right[-k-1:]
+            print(k, stop)
+            return stop
         
         def Try(k):
             for i in range(10):
